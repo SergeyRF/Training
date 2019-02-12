a376a9aefca72995ea45d3690b71389ac4b8edd8
sergey <baskervilei08@gmail.com>
2019-02-12 21:12:37 +0400

    alt+ctrl+l

--- Downloader.kt
+++ Downloader.kt
@@ -11,7 +11,7 @@ class Downloader() {
     private val imageObserver = QueueObservable<String>()
     private val webObserver = QueueObservable<String>()
     private val checkWebObserver = QueueSetObservable<String?>()
-    private val checkImageObserver =  QueueSetObservable<String?>()
+    private val checkImageObserver = QueueSetObservable<String?>()
     private lateinit var webName: String
     private val listReport = arrayListOf<Report>()
 
@@ -26,28 +26,28 @@ class Downloader() {
         return name
     }
 
-     fun start(website: String) :ArrayList<Report> {
+    fun start(website: String): ArrayList<Report> {
         webName = siteName(website)
         val service = Executors.newFixedThreadPool(4)
 
 
-        imageObserver.subscribe(object :QueueObservable.QueueObserver<String>{
+        imageObserver.subscribe(object : QueueObservable.QueueObserver<String> {
             override fun observerAdd(value: String) {
 
                 service.submit {
-                    val rep = downloadAndSaveImage(value,"${value.replace("/","").
-                            replace(".","")}image.jpg")
-                    if (rep!=null)
-                        synchronized(listReport){
+                    val rep = downloadAndSaveImage(value,
+                            "${value.replace("/", "").replace(".", "")}image.jpg")
+                    if (rep != null)
+                        synchronized(listReport) {
                             listReport.add(rep)
                         }
                 }
             }
         })
 
-        checkImageObserver.subscribe(object :QueueSetObservable.ObserverSet<String?>{
+        checkImageObserver.subscribe(object : QueueSetObservable.ObserverSet<String?> {
             override fun observerAdd(value: String?) {
-                if (value!=null) {
+                if (value != null) {
                     returnListImage(value).forEach {
                         imageObserver.addValue(it)
                     }
@@ -55,9 +55,9 @@ class Downloader() {
             }
         })
 
-        checkWebObserver.subscribe(object :QueueSetObservable.ObserverSet<String?>{
+        checkWebObserver.subscribe(object : QueueSetObservable.ObserverSet<String?> {
             override fun observerAdd(value: String?) {
-                if (value!=null) {
+                if (value != null) {
                     service.submit {
                         returnListUri(value).forEach {
                             webObserver.addValue(it)
@@ -67,98 +67,98 @@ class Downloader() {
             }
         })
 
-        webObserver.subscribe(object :QueueObservable.QueueObserver<String>{
+        webObserver.subscribe(object : QueueObservable.QueueObserver<String> {
             override fun observerAdd(value: String) {
 
-               // service.submit{
-                    val textUri = downloadUri(value)
-                    checkWebObserver.addValue(textUri)
-                    checkImageObserver.addValue(textUri)
+                // service.submit{
+                val textUri = downloadUri(value)
+                checkWebObserver.addValue(textUri)
+                checkImageObserver.addValue(textUri)
                 println("thread sleep")
                 Thread.sleep(100)
-               // }
+                // }
             }
         })
-         val textWeb = downloadUri(website)
-         checkImageObserver.addValue(textWeb)
-         checkWebObserver.addValue(textWeb)
+        val textWeb = downloadUri(website)
+        checkImageObserver.addValue(textWeb)
+        checkWebObserver.addValue(textWeb)
 
-         service.awaitTermination(10,TimeUnit.SECONDS)
-         service.shutdown()
-         println("service Shutdown \n\n\n\n\t")
-         service.awaitTermination(100,TimeUnit.SECONDS)
-         synchronized(listReport) {
-             return listReport
-         }
+        service.awaitTermination(10, TimeUnit.SECONDS)
+        service.shutdown()
+        println("service Shutdown \n\n\n\n\t")
+        service.awaitTermination(100, TimeUnit.SECONDS)
+        synchronized(listReport) {
+            return listReport
+        }
 
 
     }
 
-    private fun downloadUri(uri: String):String? {
-        try {
-            return URL(uri).readText()
+    private fun downloadUri(uri: String): String? {
+        return try {
+            URL(uri).readText()
         } catch (ex: Exception) {
             println("download Exception ")
-            return null
+            null
         }
     }
 
-        private fun returnListUri(textUri: String): ArrayList<String> {
-            val pattern3 = "(\")(http)([^\\s-]{0,})($webName)([^\\s-]{0,})(\")"
-            val pat = Pattern.compile(pattern3)
+    private fun returnListUri(textUri: String): ArrayList<String> {
+        val pattern3 = "(\")(http)([^\\s-]{0,})($webName)([^\\s-]{0,})(\")"
+        val pat = Pattern.compile(pattern3)
 
-            val matcher = pat.matcher(textUri)
-            val listUri = arrayListOf<String>()
-            while (matcher.find()) {
-                var a = matcher.group().replace("\"", "").replace(" ", "")
-                if (a.indexOf(".jpg") == -1 && a.indexOf(".png") == -1) {
+        val matcher = pat.matcher(textUri)
+        val listUri = arrayListOf<String>()
+        while (matcher.find()) {
+            var a = matcher.group().replace("\"", "").replace(" ", "")
+            if (a.indexOf(".jpg") == -1 && a.indexOf(".png") == -1) {
 
-                    if (a.indexOf("//") == 0) {
-                        a = "https:$a"
-                    }
-                    listUri.add(a)
-                    println("check web $a")
-                }
-            }
-            return listUri
-        }
-
-        private fun returnListImage(textUri: String): ArrayList<String> {
-
-            val pattern2 = "(\")([^\\s-]{0,})((\\.jpg)|(\\.png))"
-            val pat = Pattern.compile(pattern2)
-            val matcher = pat.matcher(textUri)
-            val result = arrayListOf<String>()
-            while (matcher.find()) {
-                var a = matcher.group().replace("\"", "").replace(" ", "")
                 if (a.indexOf("//") == 0) {
                     a = "https:$a"
                 }
-                result.add(a)
-
+                listUri.add(a)
+                println("any.check web $a")
             }
-            return result
         }
-
-        private fun downloadAndSaveImage(imageUri: String,
-                                         fileName: String): Report? {
-            try {
-
-                val image = ImageIO.read(URL(imageUri))
-                val time = System.currentTimeMillis()
-                val width = image.width
-                val height = image.height
-                val file = File(fileName)
-                ImageIO.write(image, "jpg", file)
-                val byte = file.length()
-                println("download image $imageUri")
-
-                return Report(time, byte, width, height, imageUri, fileName)
-
-
-            } catch (e: Exception) {
-                return null
-            }
-
-        }
+        return listUri
     }
+
+    private fun returnListImage(textUri: String): ArrayList<String> {
+
+        val pattern2 = "(\")([^\\s-]{0,})((\\.jpg)|(\\.png))"
+        val pat = Pattern.compile(pattern2)
+        val matcher = pat.matcher(textUri)
+        val result = arrayListOf<String>()
+        while (matcher.find()) {
+            var a = matcher.group().replace("\"", "").replace(" ", "")
+            if (a.indexOf("//") == 0) {
+                a = "https:$a"
+            }
+            result.add(a)
+
+        }
+        return result
+    }
+
+    private fun downloadAndSaveImage(imageUri: String,
+                                     fileName: String): Report? {
+        try {
+
+            val image = ImageIO.read(URL(imageUri))
+            val time = System.currentTimeMillis()
+            val width = image.width
+            val height = image.height
+            val file = File(fileName)
+            ImageIO.write(image, "jpg", file)
+            val byte = file.length()
+            println("download image $imageUri")
+
+            return Report(time, byte, width, height, imageUri, fileName)
+
+
+        } catch (e: Exception) {
+            return null
+        }
+
+    }
+}
